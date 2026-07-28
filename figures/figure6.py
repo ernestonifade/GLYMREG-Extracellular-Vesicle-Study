38a3b424e0311aa97d9615b3be76673df61de6c8
Ernest Onifade <148909309+ernestonifade@users.noreply.github.com>
2026-07-28 13:14:32 +0100

    Add matplotlib import to figure6.py

--- figures/figure6.py
+++ figures/figure6.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import pingouin as pg
