187f17641f85996577fed6cf0944b293b276f120
Ernest Onifade <148909309+ernestonifade@users.noreply.github.com>
2026-07-30 14:28:39 +0100

    Update figure8.py

--- figures/figure8.py
+++ figures/figure8.py
@@ -251,6 +251,7 @@ def render_figure8():
         mime=(
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         ),
+        key="download_figure_8_report",  # <--- This unique key fixes the collision
     )
 
   view_selection = st.sidebar.radio(
